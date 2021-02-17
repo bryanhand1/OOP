9a6d2c5fa3dbc66564e1e66e9906e6625e677a13
Bryan Hand <bryan_hand1@baylor.edu>
2021-02-17 10:17:58 -0600

    Finished RetailItems

---
+++ RetailItem.py
@@ -0,0 +1,8 @@
+import RetailItemClass as RI
+
+
+Item1 = RI.RetailItem("Jacket", 12, 59.95)
+
+Item2 = RI.RetailItem("Designer Jeans", 40, 34.95)
+
+Item3 = RI.RetailItem("Shirt", 20, 24.95)
---
+++ RetailItemClass.py
@@ -0,0 +1,5 @@
+class RetailItem:
+    def __init__(self, desc, unit, price):
+        self.__description = desc
+        self.__unit = unit
+        self.__price = price
